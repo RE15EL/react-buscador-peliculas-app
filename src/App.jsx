f3f1a44b7c2acddd58e03c56985f64e6276802cf
Reisel Valle <reiselvalle@gmail.com>
2023-10-11 17:28:50 -0500

    update styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
+import movies from './mocks/results.json'
 
 function App() {
+  const moviesResult = movies.Search
+  const hasMovies = moviesResult.length > 0;
 
   return (
     <div className='page'>
@@ -13,7 +16,29 @@ function App() {
       </header>
 
       <main>
-        <h3>Resultados</h3>
+        <section className='results'>
+          {
+            hasMovies 
+              ? (
+                <ul className='movie-container'>
+                  {
+                    moviesResult.map(movie => (
+                      <li key={movie.imdbID} className='movie'>
+                        <div className='movie-info'>
+                          <h4>{movie.Title}</h4>
+                          <p>{movie.Year}</p>
+                        </div>
+                        <img src={movie.Poster} alt={movie.Title} />
+                      </li>
+                    ))
+                  }
+                </ul>
+              ) 
+              : (
+                <p>No se encontraron resultados</p>
+              )
+          }
+        </section>
       </main>
     </div>
   )
