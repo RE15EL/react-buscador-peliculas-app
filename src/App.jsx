998503933fe87271bf290cca34aa58bb95f2d09f
Reisel Valle <reiselvalle@gmail.com>
2023-10-12 12:16:16 -0500

    useRef hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import './App.css'
+import { useRef } from 'react'
 import { Movies } from './components/Movies';
 import { useMovies } from './hooks/useMovies';
 
 function App() {
   const {movies} = useMovies();
+  const inputRef = useRef();
+
+  const search = (event)=> {
+    event.preventDefault();
+    const value = inputRef.current.value;
+    console.log(value);
+  }
 
   return (
     <div className='page'>
       <header>
         <h1>Buscador de peliculas</h1>
-        <form >
-          <input type="text" placeholder="Avenger, Star Wars ..."/>
+        <form onSubmit={search}>
+          <input ref={inputRef} type="text" placeholder="Avenger, Star Wars ..."/>
           <button type="submit">Buscar</button>
         </form>
       </header>
