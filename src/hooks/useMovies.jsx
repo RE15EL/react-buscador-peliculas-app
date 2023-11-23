9992d447f53fe80f30a9dbf28524b79631817276
Reisel Valle <reiselvalle@gmail.com>
2023-11-23 18:26:29 -0500

    cleaning code

--- src/hooks/useMovies.jsx
+++ src/hooks/useMovies.jsx
@@ -4,13 +4,6 @@ import { getMovies } from "../services/movies";
 export function useMovies(){
   const [term, setTerm] = useState('');
   const [movies, setMovies] = useState([]);
-    // const movies = moviesList.Search;
-    // const mappedMovies = movies?.map( movie => ({
-    //   id: movie.imdbID,
-    //   title: movie.Title,
-    //   year: movie.Year,
-    //   poster: movie.Poster,
-    // }));
 
     useEffect(()=>{
       const loadMovies = async () =>{
