6eceb88bf14f2a13d7d56888fc3ae42bd4349945
Reisel Valle <reiselvalle@gmail.com>
2023-11-23 18:59:50 -0500

    dont searching the same term

--- src/hooks/useMovies.jsx
+++ src/hooks/useMovies.jsx
@@ -1,12 +1,14 @@
-import { useEffect, useState  } from "react"
+import { useEffect, useRef, useState  } from "react"
 import { getMovies } from "../services/movies";
 
 export function useMovies(){
   const [term, setTerm] = useState('');
   const [movies, setMovies] = useState([]);
-
+  const prevTerm = useRef(term);
+  
     useEffect(()=>{
       const loadMovies = async () =>{
+        if(prevTerm.current === term) return;
         if(term.length < 1) return
 
         try {
